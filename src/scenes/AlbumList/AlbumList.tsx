03a1cbb8aca604bc3b653a7d6fa22ce7f949d189
George Metakos <gsmetak@gmail.com>
2020-11-29 00:17:03 +0200

    feat: connect data to albums component

--- src/scenes/AlbumList/AlbumList.tsx
+++ src/scenes/AlbumList/AlbumList.tsx
@@ -1,5 +1,28 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
-const AlbumList = () => <span>AlbumList component!</span>;
+import Card from '../../components/Card';
+
+import data from '../../resources/albums.json';
+
+const SContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+
+  @media (min-width: 768px) {
+    width: 60em;
+  }
+`;
+
+const { albums } = data;
+
+const AlbumList = () => (
+  <SContainer>
+    {albums.map((album) => (
+      <Card key={album.id} imageUrl={album.cover} title={album.title} />
+    ))}
+  </SContainer>
+);
 
 export default AlbumList;
