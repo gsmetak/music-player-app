fdefbc20770949d37ea808ad4b1bbfdb11be5a5c
George Metakos <gsmetak@gmail.com>
2020-11-29 01:55:44 +0200

    feat: add ListItem component

---
+++ src/components/ListItem/ListItem.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import ListItem from '.';
+
+const renderListItem = () =>
+  render(
+    <BrowserRouter>
+      <ListItem
+        artist="Track Artist"
+        imageUrl="https://picsum.photos/400/300"
+        title="Track Title"
+      />
+    </BrowserRouter>,
+  );
+
+describe('<ListItem />', () => {
+  it('will render the component', () => {
+    renderListItem();
+
+    expect(screen.getByText('by Track Artist')).toBeInTheDocument();
+    expect(screen.getByText('Track Title')).toBeTruthy();
+    expect(screen.getByTestId('item-image')).toBeTruthy();
+  });
+});
