8e521a5bfd5b16dba2a83744d7e171c5a30ad6cf
George Metakos <gsmetak@gmail.com>
2020-11-28 17:59:30 +0200

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Hello world!</p>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
