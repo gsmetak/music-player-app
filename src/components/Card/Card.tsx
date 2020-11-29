9cecfdffc1dc92498d4896164620c2b56b57ac4b
George Metakos <gsmetak@gmail.com>
2020-11-29 18:06:10 +0200

    chore: extract styled components to separate files

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,64 +1,12 @@
 import * as React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
-import { H3 } from 'components/styled';
+import { SCard, SH3, SImage, SLink } from './styled';
 
 interface Props {
   id: string;
   imageUrl: string;
   title: string;
 }
-
-const SCard = styled.div`
-  background: #fff;
-  border-radius: 0.6em;
-  box-shadow: 0 13px 27px -5px hsla(240, 30.1%, 28%, 0.25),
-    0 8px 16px -8px hsla(0, 0%, 0%, 0.3), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03);
-  cursor: pointer;
-  margin: 2em auto;
-  overflow: hidden;
-  transition: all ease 200ms;
-  width: 14vw;
-
-  &:hover {
-    transform: scale(1.03);
-    box-shadow: 0 13px 40px -5px hsla(240, 30.1%, 28%, 0.12),
-      0 8px 32px -8px hsla(0, 0%, 0%, 0.14),
-      0 -6px 32px -6px hsla(0, 0%, 0%, 0.02);
-  }
-
-  @media (max-width: 1024px) {
-    width: 18vw;
-  }
-
-  @media (max-width: 768px) {
-    width: 60vw;
-  }
-`;
-
-const SImage = styled.img`
-  width: 100%;
-  object-fit: cover;
-`;
-
-const SH3 = styled(H3)`
-  color: black;
-  padding: 1.2em;
-  text-align: center;
-`;
-
-const SLink = styled(Link)`
-  text-decoration: none;
-
-  &:focus,
-  &:hover,
-  &:visited,
-  &:link,
-  &:active {
-    text-decoration: none;
-  }
-`;
 
 const Card = ({ id, imageUrl, title }: Props) => {
   return (
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,59 +1,12 @@
 import * as React from 'react';
-import styled from 'styled-components';
 
-import { H3, H2 } from 'components/styled';
+import { SH2, SH3, SImage, SListItem } from './styled';
 
 interface Props {
   artist: string;
   imageUrl: string;
   title: string;
 }
-
-const SListItem = styled.li`
-  display: flex;
-  background: #fff;
-  border-radius: 0.6em;
-  box-shadow: 0 13px 27px -5px hsla(240, 30.1%, 28%, 0.25),
-    0 8px 16px -8px hsla(0, 0%, 0%, 0.3), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03);
-  cursor: pointer;
-  margin: 1em auto;
-  padding: 5px;
-  overflow: hidden;
-  transition: all ease 200ms;
-  width: 50vw;
-
-  &:hover {
-    transform: scale(1.03);
-    box-shadow: 0 13px 40px -5px hsla(240, 30.1%, 28%, 0.12),
-      0 8px 32px -8px hsla(0, 0%, 0%, 0.14),
-      0 -6px 32px -6px hsla(0, 0%, 0%, 0.02);
-  }
-
-  @media (max-width: 768px) {
-    width: 85vw;
-    padding: 10px 5px;
-  }
-`;
-
-const SImage = styled.img`
-  border-radius: 0.6em;
-  width: 4vw;
-  object-fit: cover;
-
-  @media (max-width: 768px) {
-    width: 15vw;
-  }
-`;
-
-const SH3 = styled(H3)`
-  color: black;
-  margin: auto 20px;
-`;
-
-const SH2 = styled(H2)`
-  color: black;
-  margin: auto 10px;
-`;
 
 const ListItem = ({ artist, imageUrl, title }: Props) => {
   return (
