e15aeb1b81ef2e7292a6bd410b0184d4cd060809
George Metakos <gsmetak@gmail.com>
2020-11-29 23:36:49 +0200

    test: update footer test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -3,17 +3,19 @@ import { render, screen } from '@testing-library/react';
 
 import Footer from './Footer';
 
-const renderFooter = () =>
-  render(
-    <Footer>
-      <span>Test</span>
-    </Footer>,
-  );
+const renderFooter = (children: React.ReactNode) =>
+  render(<Footer>{children}</Footer>);
 
 describe('<Footer />', () => {
   it('will render the component', () => {
-    renderFooter();
+    renderFooter(<span>Test</span>);
 
     expect(screen.getByTestId('app-footer')).toBeTruthy();
+  });
+
+  it('will render the children', () => {
+    renderFooter(<span data-test="test-child">Test</span>);
+
+    expect(screen.getByTestId('test-child')).toBeTruthy();
   });
 });
