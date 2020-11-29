6c9fbb8acfa8bf30275075a42708918dd050e3c7
George Metakos <gsmetak@gmail.com>
2020-11-29 23:34:51 +0200

    test: update progressbar tests

--- src/components/MusicPlayer/components/ProgressBar/ProgressBar.test.tsx
+++ src/components/MusicPlayer/components/ProgressBar/ProgressBar.test.tsx
@@ -2,13 +2,19 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import ProgressBar from './ProgressBar';
+import secondsToMinutesAndSeconds from './utils';
 
-const renderProgressBar = () => render(<ProgressBar />);
+const renderProgressBar = () =>
+  render(<ProgressBar currentTime={0} duration={1} seekTo={() => {}} />);
 
-describe('<Footer />', () => {
+describe('<ProgressBar />', () => {
   it('will render the component', () => {
     renderProgressBar();
 
     expect(screen.getByTestId('progress-bar')).toBeTruthy();
+  });
+
+  it('will return a correctly formatted time', () => {
+    expect(secondsToMinutesAndSeconds(60)).toEqual('1:00');
   });
 });
